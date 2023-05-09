8c8b993ed56910d8ef3d488c29803e296f1cf847
janit <janit@LAPTOP-V61MSFIM>
2023-05-08 21:37:45 -0500

    prueba2

--- Taller_PruebasAPI_DDP-main/proyecto_solution/AppDDP/test_main.py
+++ Taller_PruebasAPI_DDP-main/proyecto_solution/AppDDP/test_main.py
@@ -1,10 +1,13 @@
 import pytest
 from fastapi import Response as rs 
-from main import  index 
+from main import  verificar_primo 
 
 
-def test_helloFastApi():
+def test_primo():
 
-    assert index() == {"mensaje": "Hello FastAPI"}
+    assert verificar_primo(5, rs ) == {'respuesta': True, 'validacion': 'Solicitud Exitosa'}
+
+def test_no_primo():
+    assert verificar_primo(6, rs) == {'respuesta': False, 'validacion': 'Solicitud Exitosa'}   
     
  
