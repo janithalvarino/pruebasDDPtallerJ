61d9220d0a504333c078ee2fd36dd38bdef80527
janit <janit@LAPTOP-V61MSFIM>
2023-05-08 23:35:19 -0500

    prueba3

--- Taller_PruebasAPI_DDP-main/proyecto_solution/AppDDP/test_main.py
+++ Taller_PruebasAPI_DDP-main/proyecto_solution/AppDDP/test_main.py
@@ -1,13 +1,7 @@
 import pytest
 from fastapi import Response as rs 
-from main import  verificar_primo 
+from main import  fibonacci
 
 
-def test_primo():
-
-    assert verificar_primo(5, rs ) == {'respuesta': True, 'validacion': 'Solicitud Exitosa'}
-
-def test_no_primo():
-    assert verificar_primo(6, rs) == {'respuesta': False, 'validacion': 'Solicitud Exitosa'}   
-    
- 
+def test_fibonacci():
+    assert fibonacci(5 , rs) == {"validacion": "Solicitud Exitosa", "respuesta":5 }  
